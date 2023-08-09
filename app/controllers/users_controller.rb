5ee0abf409c876906a741da833f6f2d631d642ff
sasaki <sasaki2931@gmail.com>
2023-08-09 05:17:24 +0000

    passwordハッシュ化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def index
+    @useer = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :introduce,length: { in: 50..200 }
+  has_secure_password
 end
