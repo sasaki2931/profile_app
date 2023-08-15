7e82ca25ce5fd20bcc62c81c5e2fcb001c318105
sasaki <sasaki2931@gmail.com>
2023-08-15 06:01:26 +0000

    コントローラー作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,38 @@
 class CategoriesController < ApplicationController
+
+
+  def new
+     @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      redirect_to category_path(@category.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @categories = Category.all
+  end
+
+  def destroy
+     @category.destroy
+     redirect_to categories_path, notice:"ブログを削除しました！"
+  end
+
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name )
+    end
+
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
 end
