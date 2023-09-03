2a98868c5aa99a6350f8e4e24d9e8b1bc4265ec8
sasaki <sasaki2931@gmail.com>
2023-09-03 08:04:50 +0000

    クリエイトアクション修正

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,24 +2,25 @@ class SkillsController < ApplicationController
   before_action :set_skill, only: [:new,:edit,:destroy]
 
   def new
+    @user = current_user
     @skill = Skill.new
-
+    @category_id = params[:category_id]
   end
 
-  def create
-  @categories = Category.all
-  @skill = Skill.new(skill_params) # 新しいスキルを作成するだけで保存はしない
+ def create
 
-    if @skill.save
+  @skill = current_user.skills.build(skill_params)
+  if @skill.save
     # スキルが正常に作成された場合
-      redirect_to edit_skill_path(@skill) # 新しいスキルの編集ページにリダイレクト
-    else
-    # スキルの作成に失敗した場合
+    # カテゴリーにスキルを関連付ける
+    redirect_to edit_skill_path(@skill) # 新しいスキルの編集ページにリダイレクト
+  else
+    #errors = @skill.errors.full_messages
+    #puts errors
+    binding.pry # スキルの作成に失敗した場合
     render :new
-    end
   end
-
-
+ end
 
 
 
@@ -57,7 +58,7 @@ class SkillsController < ApplicationController
   private
 
     def skill_params
-      params.require(:skill).permit(:name, :level, :category_id)
+      params.require(:skill).permit(:name, :level,:category_id)
     end
 
     def set_skill
